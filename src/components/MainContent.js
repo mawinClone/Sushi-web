bdf72e5a9de59556cb5871ba5ab08dbb761de0df
RafahCTR <rafahphanthila@gmail.com>
2023-12-17 12:49:17 +0700

    sushi image

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,18 +3,20 @@ const MainContent = () => {
   return (
     <section>
       <div className="container">
-      <div className="maincontent">
-        <div className="maincontent-info">
-          <h1>Special Story !!!</h1>
-          <p>In the realm of culinary pleasures, sushi stands out as a timeless and exquisite delicacy that captivates the taste buds with its artful blend of flavors and textures. To elevate the dining experience, many sushi establishments strategically introduce enticing promotions, making the gastronomic journey even more delightful. This article delves into the world of sushi promotions, highlighting the creativity and innovation that restaurateurs employ to tantalize the palates of sushi enthusiasts.</p>
-        </div>
-        <div className="maincontent-image">
-          <img src="/Images/andraz-lazic-iy_MT2ifklc-unsplash.jpg" alt="sushi"></img>
+        <div className="maincontent">
+          <div className="maincontent-info">
+            <h1>Special Story !!!</h1>
+            <div className="crop-text">
+              <p>In the realm of culinary pleasures, sushi stands out as a timeless and exquisite delicacy that captivates the taste buds with its artful blend of flavors and textures. To elevate the dining experience, many sushi establishments strategically introduce enticing promotions, making the gastronomic journey even more delightful. This article delves into the world of sushi promotions, highlighting the creativity and innovation that restaurateurs employ to tantalize the palates of sushi enthusiasts.</p>
+            </div>
+          </div>
+          <div className="maincontent-image">
+            <img src="/Images/andraz-lazic-iy_MT2ifklc-unsplash.jpg" alt="sushi"></img>
+          </div>
         </div>
       </div>
-    </div>
     </section>
-    
+
   );
 };
 export default MainContent;
--- src/data/sushiData.js
+++ src/data/sushiData.js
@@ -16,36 +16,72 @@ const sushiData = [
         thumbnailURL:"/images/pexels-olga-lioncat-7245479.jpg"
     },
     {
-        title: "sushi 01",
-        thumbnailURL:"/images/pexels-isabella-mendes-858496.jpg"
+        title: "sushi 05",
+        thumbnailURL:"/images/ai-generated-7955586_640.jpg"
     },
     {
-        title: "sushi 02",
-        thumbnailURL:"/images/pexels-ivan-samkov-8951267.jpg"
+        title: "sushi 06",
+        thumbnailURL:"/images/food-1406883_640.jpg"
     },
     {
-        title: "sushi 03",
-        thumbnailURL:"/images/pexels-kim-cruz-4071397.jpg"
+        title: "sushi 07",
+        thumbnailURL:"/images/food-3199433_640.jpg"
     },
     {
-        title: "sushi 04",
-        thumbnailURL:"/images/pexels-olga-lioncat-7245479.jpg"
+        title: "sushi 08",
+        thumbnailURL:"/images/food-4863582_640.jpg"
     },
     {
-        title: "sushi 01",
-        thumbnailURL:"/images/pexels-isabella-mendes-858496.jpg"
+        title: "sushi 09",
+        thumbnailURL:"/images/japanese-cuisine-6861681_640.jpg"
     },
     {
-        title: "sushi 02",
-        thumbnailURL:"/images/pexels-ivan-samkov-8951267.jpg"
+        title: "sushi 10",
+        thumbnailURL:"/images/prawn-250868_640.jpg"
     },
     {
-        title: "sushi 03",
-        thumbnailURL:"/images/pexels-kim-cruz-4071397.jpg"
+        title: "sushi 11",
+        thumbnailURL:"/images/restaurant-4165427_640.jpg"
     },
     {
-        title: "sushi 04",
-        thumbnailURL:"/images/pexels-olga-lioncat-7245479.jpg"
+        title: "sushi 12",
+        thumbnailURL:"/images/rice-3199406_640.jpg"
+    },
+    {
+        title: "sushi 13",
+        thumbnailURL:"/images/restaurant-4306570_640.jpg"
+    },
+    {
+        title: "sushi 14",
+        thumbnailURL:"/images/salmon-1353598_640.jpg"
+    },
+    {
+        title: "sushi 15",
+        thumbnailURL:"/images/sushi-748132_640.jpg"
+    },
+    {
+        title: "sushi 16",
+        thumbnailURL:"/images/sushi-2362984_640.jpg"
+    },
+    {
+        title: "sushi 17",
+        thumbnailURL:"/images/sushi-2373302_640.jpg"
+    },
+    {
+        title: "sushi 18",
+        thumbnailURL:"/images/sushi-2584587_640.jpg"
+    },
+    {
+        title: "sushi 19",
+        thumbnailURL:"/images/sushi-4854696_640.jpg"
+    },
+    {
+        title: "sushi 20",
+        thumbnailURL:"/images/sushi-5902131_640.jpg"
     },
 ];
+
+// var fs = require('fs');
+// var files = fs.readdirSync('/images/');
+// console.log(files);
 export default sushiData;
